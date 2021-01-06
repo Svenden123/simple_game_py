1ac5ab6d1d66217b7540ddd08334cfd2b83b8a88
Svenden123 <buhrus1@yandex.ru>
2021-01-06 19:54:35 +0400

    first commit

---
+++ index.py
@@ -0,0 +1,7 @@
+color = input("Enter your color: ")
+plural_noun = input("Enter your plural noun: ")
+celebrity = input("Enter your celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
